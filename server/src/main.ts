cb649d1b480eeeba092665760ad44cf1f2aa14d0
Kyeongho Yang <88yangkh@gmail.com>
2022-02-04 23:22:15 +0900

    :wrench: 백엔드 API Global Prefix `/api` 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/app.module";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
+import { APP } from "@/constants";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
 
+  app.setGlobalPrefix(APP.GLOBAL_PREFIX);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
