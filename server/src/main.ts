8577e0f0199aa4cf57c60f711a032d8d7f1675fd
Kyeongho Yang <88yangkh@gmail.com>
2022-02-07 07:46:04 +0900

    :art: main.ts 싱글톤 패턴 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,18 +2,33 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/app.module";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 import { APP } from "@/constants";
-import { HttpExceptionFilter } from "@/common";
+import { HttpExceptionFilter, SuccessInterceptor } from "@/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+class Application {
+  constructor(private app: NestExpressApplication) {
+    this.app = app;
+  }
+
+  private async setUpGlobalMiddleware() {
+    this.app.useGlobalInterceptors(new SuccessInterceptor());
+    this.app.useGlobalFilters(new HttpExceptionFilter());
+    this.app.useLogger(this.app.get(WINSTON_MODULE_NEST_PROVIDER));
+  }
+
+  async bootstrap() {
+    this.app.setGlobalPrefix(APP.GLOBAL_PREFIX);
+    await this.setUpGlobalMiddleware();
+    await this.app.listen(process.env.PORT);
+  }
+}
+
+async function init(): Promise<void> {
+  const server = await NestFactory.create<NestExpressApplication>(AppModule, {
     bufferLogs: true,
   });
-
-  app.setGlobalPrefix(APP.GLOBAL_PREFIX);
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-
-  app.useGlobalFilters(new HttpExceptionFilter());
-
-  await app.listen(process.env.PORT);
+  const app = new Application(server);
+  await app.bootstrap();
 }
-bootstrap();
+
+init();
