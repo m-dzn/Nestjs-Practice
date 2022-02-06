afe14c10f05d262c1e4c6376d042eb793d276907
Kyeongho Yang <88yangkh@gmail.com>
2022-02-06 23:40:45 +0900

    :recycle: 예외 일괄처리를 위한 HTTP Exception 필터 추가

---
+++ server/src/common/filter/http-exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from "@nestjs/common";
+import { Response } from "express";
+import { ErrorResponse } from "../interface";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const error = exception.getResponse() as string | ErrorResponse;
+
+    const commonResponse = {
+      success: false,
+      status,
+    };
+
+    if (typeof error === "string") {
+      res.status(status).json({
+        ...commonResponse,
+        message: error,
+      });
+    } else {
+      res.status(status).json({ ...commonResponse, ...error });
+    }
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/app.module";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 import { APP } from "@/constants";
+import { HttpExceptionFilter } from "@/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix(APP.GLOBAL_PREFIX);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT);
 }
