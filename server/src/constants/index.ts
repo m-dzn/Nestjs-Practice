f405be582be877f79a00617126ac8842afcb2061
Kyeongho Yang <88yangkh@gmail.com>
2022-02-07 08:18:04 +0900

    :heavy_plus_sign: Swagger API 문서 라이브러리 추가

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,3 +1,12 @@
+export * from "./messages";
+
 export const APP = {
-  GLOBAL_PREFIX: "/api/",
+  GLOBAL_PREFIX: "api",
+  VERSION: "0.0.1",
+  SWAGGER: {
+    PREFIX: "/docs",
+    PREFIX_JSON: "/docs-json",
+    TITLE: "VIA - API",
+    DESCRIPTION: "VIA Fashion E-Commerce API Docs",
+  },
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,55 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "@/app.module";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as expressBasicAuth from "express-basic-auth";
+
 import { APP } from "@/constants";
 import { HttpExceptionFilter, SuccessInterceptor } from "@/common";
-import { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
 
 class Application {
+  private ADMIN_USER: string;
+  private ADMIN_PASSWORD: string;
+
   constructor(private app: NestExpressApplication) {
     this.app = app;
+
+    this.ADMIN_USER = process.env.ADMIN_USER;
+    this.ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+  }
+
+  private setUpBasicAuth() {
+    this.app.use(
+      [APP.SWAGGER.PREFIX, APP.SWAGGER.PREFIX_JSON],
+      expressBasicAuth({
+        challenge: true,
+        users: {
+          [this.ADMIN_USER]: this.ADMIN_PASSWORD,
+        },
+      })
+    );
+  }
+
+  private setUpOpenAPIMiddleware() {
+    SwaggerModule.setup(
+      APP.SWAGGER.PREFIX,
+      this.app,
+      SwaggerModule.createDocument(
+        this.app,
+        new DocumentBuilder()
+          .setTitle(APP.SWAGGER.TITLE)
+          .setDescription(APP.SWAGGER.DESCRIPTION)
+          .setVersion(APP.VERSION)
+          .build()
+      )
+    );
   }
 
   private async setUpGlobalMiddleware() {
+    this.setUpBasicAuth();
+    this.setUpOpenAPIMiddleware();
+
     this.app.useGlobalInterceptors(new SuccessInterceptor());
     this.app.useGlobalFilters(new HttpExceptionFilter());
     this.app.useLogger(this.app.get(WINSTON_MODULE_NEST_PROVIDER));
