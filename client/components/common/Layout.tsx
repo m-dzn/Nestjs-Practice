53079c53ce3cbb6b3ee58c96620d571ed044c4c1
Kyeongho Yang <88yangkh@gmail.com>
2022-02-08 16:53:21 +0900

    :lipstick: 공통 Layout UI 컴포넌트에 스타일 추가

--- client/components/common/Layout.tsx
+++ client/components/common/Layout.tsx
@@ -1,8 +1,20 @@
-import styled from "@emotion/styled";
 import { HTMLAttributes, memo } from "react";
+import styled from "@emotion/styled";
+import { styles } from "@/styles";
+
+const LAYOUT = {
+  style: {
+    padding: `0 ${styles.space.level3}rem`,
+    rowGap: 4,
+  },
+};
 
 const Container = styled.div`
   min-height: inherit;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: ${LAYOUT.style.rowGap}rem;
 `;
 
 interface Props extends HTMLAttributes<HTMLDivElement> {}
