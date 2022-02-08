f8f62fd934406b1599d082b05078923981531933
Kyeongho Yang <88yangkh@gmail.com>
2022-02-08 19:23:41 +0900

    :bento: 카카오, 네이버 로고 이미지 추가

--- client/constants/images.ts
+++ client/constants/images.ts
@@ -2,4 +2,6 @@ const setImagePrefix = (path: string) => `/images${path}`;
 
 export default {
   logo: setImagePrefix("/temp-logo.png"),
+  kakaoLogo: setImagePrefix("/kakao-logo.png"),
+  naverLogo: setImagePrefix("/naver-logo.png"),
 };
