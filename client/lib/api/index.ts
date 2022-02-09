db46735eed779e8d9eac021965015e9c9f4446e5
Kyeongho Yang <88yangkh@gmail.com>
2022-02-09 10:26:39 +0900

    :wrench: Next.js API Proxy 설정

--- client/lib/api/index.ts
+++ client/lib/api/index.ts
@@ -1,7 +1,3 @@
 import Axios from "axios";
 
-console.log(process.env.API_BASE_URL);
-
-export const axios = Axios.create({
-  baseURL: process.env.API_BASE_URL,
-});
+export const axios = Axios.create();
