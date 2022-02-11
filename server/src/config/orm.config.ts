cde84076f07dc6aa07629906b26b5db54e9b7d6b
Kyeongho Yang <88yangkh@gmail.com>
2022-02-11 21:22:54 +0900

    :recycle: TypeORM 환경 변수 상수로 관리, 중복 제거

--- server/src/config/orm.config.ts
+++ server/src/config/orm.config.ts
@@ -1,15 +1,17 @@
+import { APP } from "@/constants";
 import { ConfigService } from "@nestjs/config";
 import { ConnectionOptions, getMetadataArgsStorage } from "typeorm";
 
 export const getOrmConfig = (config: ConfigService): ConnectionOptions => ({
   type: "mysql",
-  host: config.get<string>("TYPEORM_HOST"),
-  port: config.get<number>("TYPEORM_PORT"),
-  database: config.get<string>("TYPEORM_DATABASE"),
-  username: config.get<string>("TYPEORM_USERNAME"),
-  password: config.get<string>("TYPEORM_PASSWORD"),
+  host: config.get<string>(APP.ENV.DB_HOST),
+  port: config.get<number>(APP.ENV.DB_PORT),
+  database: config.get<string>(APP.ENV.DB_DATABASE),
+  username: config.get<string>(APP.ENV.DB_USERNAME),
+  password: config.get<string>(APP.ENV.DB_PASSWORD),
   entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
-  synchronize: true,
+  synchronize:
+    config.get<string>(APP.ENV.NODE_ENV) === APP.ENV.NODE_ENV_DEVELOPMENT,
   logging: true,
   charset: "utf8mb4_unicode_ci",
 });
