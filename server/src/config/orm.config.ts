fa80931183fde8a3abc3d96a0ee3c7c9b81aa35a
Kyeongho Yang <88yangkh@gmail.com>
2022-02-11 21:33:06 +0900

    :recycle: TypeORM NODE_ENV별 조건부 설정 코드 중복 제거

--- server/src/config/orm.config.ts
+++ server/src/config/orm.config.ts
@@ -2,16 +2,20 @@ import { APP } from "@/constants";
 import { ConfigService } from "@nestjs/config";
 import { ConnectionOptions, getMetadataArgsStorage } from "typeorm";
 
-export const getOrmConfig = (config: ConfigService): ConnectionOptions => ({
-  type: "mysql",
-  host: config.get<string>(APP.ENV.DB_HOST),
-  port: config.get<number>(APP.ENV.DB_PORT),
-  database: config.get<string>(APP.ENV.DB_DATABASE),
-  username: config.get<string>(APP.ENV.DB_USERNAME),
-  password: config.get<string>(APP.ENV.DB_PASSWORD),
-  entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
-  synchronize:
-    config.get<string>(APP.ENV.NODE_ENV) === APP.ENV.NODE_ENV_DEVELOPMENT,
-  logging: true,
-  charset: "utf8mb4_unicode_ci",
-});
+export const getOrmConfig = (config: ConfigService): ConnectionOptions => {
+  const isDevEnv =
+    config.get<string>(APP.ENV.NODE_ENV) === APP.ENV.NODE_ENV_DEVELOPMENT;
+
+  return {
+    type: "mysql",
+    host: config.get<string>(APP.ENV.DB_HOST),
+    port: config.get<number>(APP.ENV.DB_PORT),
+    database: config.get<string>(APP.ENV.DB_DATABASE),
+    username: config.get<string>(APP.ENV.DB_USERNAME),
+    password: config.get<string>(APP.ENV.DB_PASSWORD),
+    entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
+    synchronize: isDevEnv,
+    logging: isDevEnv,
+    charset: "utf8mb4_unicode_ci",
+  };
+};
